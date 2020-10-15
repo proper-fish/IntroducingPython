aebae83391ac3e32714b7b107fec356cf00f3b86
Aliona Kozushkina <aliona.kozushkina@gmail.com>
2020-10-15 23:49:21 +0300

    Starting on Unicode strings

---
+++ data.py
@@ -0,0 +1,7 @@
+# unicode strings
+
+def unicode_test(value):
+    import unicodedata
+    name = unicodedata.name(value)
+    value2 = unicodedata.lookup(name)
+    print('value="%s", name="%s", value2="%s"' % (value, name, value2))
