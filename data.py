7ec9d927b11d5f4897c3a9b2f751283609bef06f
Aliona Kozushkina <aliona.kozushkina@gmail.com>
2020-10-22 00:03:30 +0300

    Added exercises on regular expressions

--- data.py
+++ data.py
@@ -28,3 +28,35 @@ def print_new():
     response = {'salutation': 'Henry', 'name': 'Finn', 'product': 'vacuum', 'verbed': 'beeped', 'room': 'bedroom', \
         'animals': 'flies', 'amount': 19, 'percent': 23, 'spokesman': 'Alona', 'job_title': 'unemployed'}
     print(letter.format(response))
+
+# regular expressions
+def reg_exp():
+    import re
+    mammoth = """We have seen thee, queen of cheese,
+    Lying quietly at your ease,
+    Gently fanned by evening breeze,
+    Thy fair form no flies dare seize.
+    All gaily dressed soon you'll go
+    To the great Provincial show,
+    To be admired by many a beau
+    In the city of Toronto.
+    Cows numerous as a swarm of bees,
+    Or as the leaves upon the trees,
+    It did require to make thee please,
+    And stand unrivalled, queen of cheese.
+    May you not receive a scar as
+    We have heard that Mr. Harris
+    Intends to send you off as far as
+    The great world's show at Paris.
+    Of the youth beware of these,
+    For some of them might rudely squeeze
+    And bite your cheek, then songs or glees
+    We could not sing, oh! queen of cheese.
+    We'rt thou suspended from balloon,
+    You'd cast a shade even at noon,
+    Folks would think it was the moon
+    About to fall and crush them soon."""
+    print("All the words that start with 'c': {}".format(re.findall(r'\bc\w*', mammoth)))
+    print("All the 4-letter words that start with 'c': {}".format(re.findall(r'\bc\w{3}\b', mammoth)))
+    print("All the words that end with 'r': {}".format(re.findall(r'\b\w{3}r\b', mammoth)))
+    print("All the words that have 3 vowels in a row: {}".format(re.findall(r'\b\w*[aeoui]{3}[^aeoui\s]*\w*\b', mammoth)))
